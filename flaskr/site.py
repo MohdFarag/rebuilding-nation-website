0c3ae155b4a116a247e1a54d2a2c2a70cc0df6a7
MohdFarag <mohamed.mahmoud02@eng-st.cu.edu.eg>
2023-04-23 14:44:13 +0200

    ADD ADMIN

--- flaskr/site.py
+++ flaskr/site.py
@@ -169,6 +169,185 @@ def article():
 
 #--------------------------------------------------------------------------#
 
+#--------------------------------------------------------------------------#
+
+# Admin Page
+@app.route("/admin", methods=['GET', 'POST'])
+def admin():
+    db_tables = database.retrieve_tables(myCursor, "*")
+    settings = db_tables['settings']
+
+    if request.method == 'POST':
+      name  = request.form['name']
+      title  = request.form['title']
+      username  = request.form['username']
+      password  = request.form['password']
+
+      myCursor.execute("""
+      UPDATE settings
+      SET title=%s, cover_text=%s, admin_username=%s, admin_password=%s
+      WHERE id=1;
+      """,(name,title,username,password))
+      mydb.commit()
+
+    return render_template("admin/index.html",
+                  name=settings[0][1],
+                  title="لوحة التحكم",
+                  settings=settings[0])
+
+# List of Books Page
+@app.route("/admin/books")
+def adminBooks():
+    db_tables = database.retrieve_tables(myCursor, "*")
+    settings = db_tables['settings']
+
+    myCursor.execute("SELECT `id`, `name`, LEFT(`description`,100), `img`, `link`, `created_at` FROM book Order by created_at DESC")
+    books = myCursor.fetchall()
+
+    return render_template("admin/books.html",
+                  name=settings[0][1],
+                  title="قائمة الكتب",
+                  settings=settings[0],
+                  books=books)
+
+# Add Book Page
+@app.route("/admin/addBook", methods=['GET', 'POST'])
+def addBook():     
+      db_tables = database.retrieve_tables(myCursor, "*")
+      settings = db_tables['settings']
+      status = -1
+      
+      if request.method == 'POST':
+        name  = request.form['name']
+        description  = request.form['description']
+        image  = request.files['image']
+        image_path = saveFile(image, name, "image") 
+        link  = request.files['link']
+        link_path = saveFile(link, name, "link") 
+        createdAt = pd.to_datetime("today")
+        createdAt = f"{createdAt.year}-{createdAt.month}-{createdAt.day}"
+      
+        myCursor.execute("""INSERT INTO Book(name, description, img, link, created_at) VALUES (%s,%s,%s,%s,%s)""",
+                                            (name, description, image_path, link_path, createdAt))
+        status = 1
+        mydb.commit() # Work Is DONE
+
+      return render_template("admin/addbook.html",
+                    name=settings[0][1],
+                    title="إضافة كتاب",
+                    settings=settings[0],
+                    status=status)
+
+# Remove Book Page
+@app.route("/admin/RemoveBook", methods=['GET', 'POST'])
+def removeBook():
+    id = argsGet("id")
+    myCursor.execute("""DELETE FROM Book WHERE id=%s""",(id,))
+    mydb.commit() # Work Is DONE
+
+    return redirect(url_for('adminBooks'))
+
+# Edit Book Page
+@app.route("/admin/EditBook", methods=['GET', 'POST'])
+def editBook():
+    db_tables = database.retrieve_tables(myCursor, "*")
+    settings = db_tables['settings']
+
+    id = argsGet("id")
+    myCursor.execute(f"""SELECT * FROM Book WHERE id={id}""")
+    book = myCursor.fetchone()
+
+    if request.method == 'POST':
+        name  = request.form['name']
+        description  = request.form['description']
+        image  = request.files['image']
+        image_path = saveFile(image, name, "image")
+        link  = request.files['link']
+        link_path = saveFile(link, name, "link") 
+        createdAt = pd.to_datetime("today")
+        createdAt = f"{createdAt.year}-{createdAt.month}-{createdAt.day}"
+
+        myCursor.execute(f"""UPDATE book SET name='{name}', description='{description}', img='{image_path}', link='{link_path}',created_at='{createdAt}' WHERE id={id}""")
+        mydb.commit()
+
+    return render_template("admin/editbook.html",
+                name=settings[0][1],
+                title="تعديل كتاب",
+                settings=settings[0],
+                book=book)
+
+# List of Articles Page
+@app.route("/admin/articles")
+def adminArticles():
+    db_tables = database.retrieve_tables(myCursor, "*")
+    settings = db_tables['settings']
+
+    myCursor.execute("SELECT `id`, `name`, LEFT(`text`,100), `created_at` FROM article Order by created_at DESC")
+    articles = myCursor.fetchall()
+
+    return render_template("admin/articles.html",
+                name=settings[0][1],
+                title="قائمة المقالات",
+                settings=settings[0],
+                articles=articles)
+
+# Add Book Page
+@app.route("/admin/addArticle", methods=['GET', 'POST'])
+def addArticle():     
+    db_tables = database.retrieve_tables(myCursor, "*")
+    settings = db_tables['settings']
+    status = -1
+    
+    if request.method == 'POST':
+      name  = request.form['name']
+      text  = request.form['text']
+      createdAt = pd.to_datetime("today")
+      createdAt = f"{createdAt.year}-{createdAt.month}-{createdAt.day}"
+    
+      myCursor.execute("""INSERT INTO Article(name, text, created_at) VALUES (%s,%s,%s)""",
+                                          (name, text, createdAt))
+      status = 1
+      mydb.commit() # Work Is DONE
+
+    return render_template("admin/addArticle.html",
+                  name=settings[0][1],
+                  title="إضافة مقال",
+                  settings=settings[0],
+                  status=status)
+
+# Remove Book Page
+@app.route("/admin/RemoveArticle", methods=['GET', 'POST'])
+def removeArticle():
+    id = argsGet("id")
+    myCursor.execute("""DELETE FROM Article WHERE id=%s""",(id,))
+    mydb.commit() # Work Is DONE
+
+    return redirect(url_for('adminArticles'))
+
+# Edit Book Page
+@app.route("/admin/EditArticle", methods=['GET', 'POST'])
+def editArticle():
+    db_tables = database.retrieve_tables(myCursor, "*")
+    settings = db_tables['settings']
+
+    id = argsGet("id")
+    myCursor.execute(f"""SELECT * FROM Article WHERE id={id}""")
+    article = myCursor.fetchone()
+
+    if request.method == 'POST':
+      name  = request.form['name']
+      text  = request.form['text']
+      createdAt = pd.to_datetime("today")
+      createdAt = f"{createdAt.year}-{createdAt.month}-{createdAt.day}"
+
+      myCursor.execute(f"""UPDATE article SET name='{name}', text='{text}', created_at='{createdAt}' WHERE id={id}""")
+      mydb.commit()
+
+    return render_template("admin/editArticle.html",
+                  name=settings[0][1],
+                  title="تعديل كتاب",
+                  settings=settings[0],
+                  article=article)
 
 #-----------Error Handler-----------#
 
